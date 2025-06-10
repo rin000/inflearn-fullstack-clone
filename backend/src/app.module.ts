fdb09b520218d3a22917674ab98d5eb0edb6489b
rin000 <engo1@naver.com>
2025-06-10 19:47:18 +0900

    Custom Prisma Model 추가

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,20 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-<<<<<<< HEAD
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-  ],
-=======
-
-@Module({
-  imports: [],
->>>>>>> 1ba154e786fd56397c565bf27e6e97caeeff4dbd
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
